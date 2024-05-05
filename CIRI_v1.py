f6d060a57828c15d6ea6648931a724c8e62eb1ce
clement1938 <52299436+clement1938@users.noreply.github.com>
2024-05-05 16:39:50 +0200

    Update CIRI_v7.py
    Avec notre réseau de neurone codé à la mano

--- CIRI_v1.py
+++ CIRI_v1.py
@@ -1,11 +1,129 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score, log_loss
+from tqdm import tqdm
+import matplotlib.pyplot as plt
 import scipy.special as sp
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
 
-#%%
+#%% Partie réseau neuronaux
+
+def initialisation(dimensions):
+    
+    parametres = {}
+    C = len(dimensions)
+
+    np.random.seed(1)
+
+    for c in range(1, C):
+        parametres['W' + str(c)] = np.random.randn(dimensions[c], dimensions[c - 1])
+        parametres['b' + str(c)] = np.random.randn(dimensions[c], 1)
+
+    return parametres
+
+def forward_propagation(X, parametres): # , activation):
+  
+  activations = {'A0': X}
+
+  C = len(parametres) // 2
+
+  for c in range(1, C + 1):
+
+    Z = parametres['W' + str(c)].dot(activations['A' + str(c - 1)]) + parametres['b' + str(c)]
+    activations['A' + str(c)] = 1 / (1 + np.exp(-Z))
+
+    # if activation == 'sigmoid':
+    #     activations['A' + str(c)] = sigmoid(Z)
+    # if activation == 'ReLU':
+    #     activations['A' + str(c)] = ReLU(Z)
+
+  return activations
+
+# def sigmoid(z):
+#     return 1 / (1 + np.exp(-z))
+
+# def ReLU(z):
+#     if z<0:
+#         return 0
+#     return z
+
+
+def back_propagation(y, parametres, activations):
+
+  m = y.shape[1]
+  C = len(parametres) // 2
+
+  dZ = activations['A' + str(C)] - y
+  gradients = {}
+
+  for c in reversed(range(1, C + 1)):
+    gradients['dW' + str(c)] = 1/m * np.dot(dZ, activations['A' + str(c - 1)].T)
+    gradients['db' + str(c)] = 1/m * np.sum(dZ, axis=1, keepdims=True)
+    if c > 1:
+      dZ = np.dot(parametres['W' + str(c)].T, dZ) * activations['A' + str(c - 1)] * (1 - activations['A' + str(c - 1)])
+
+  return gradients
+
+def update(gradients, parametres, learning_rate):
+
+    C = len(parametres) // 2
+
+    for c in range(1, C + 1):
+        parametres['W' + str(c)] = parametres['W' + str(c)] - learning_rate * gradients['dW' + str(c)]
+        parametres['b' + str(c)] = parametres['b' + str(c)] - learning_rate * gradients['db' + str(c)]
+
+    return parametres
+
+def predict(X, parametres):
+  activations = forward_propagation(X, parametres)
+  C = len(parametres) // 2
+  Af = activations['A' + str(C)]
+  return Af >= 0.5
+
+def deep_neural_network(X, y, hidden_layers = (128, 64, 32), learning_rate = 0.01, n_iter = 2**12):
+    
+    # initialisation parametres
+    dimensions = list(hidden_layers)
+    dimensions.insert(0, X.shape[0])
+    dimensions.append(y.shape[0])
+    np.random.seed(1)
+    parametres = initialisation(dimensions)
+
+    # tableau numpy contenant les futures accuracy et log_loss
+    training_history = np.zeros((int(n_iter), 2))
+
+    C = len(parametres) // 2
+
+    # gradient descent
+    for i in tqdm(range(n_iter)):
+        
+        # X_bruite = X+bruit
+        activations = forward_propagation(X, parametres)
+        gradients = back_propagation(y, parametres, activations)
+        parametres = update(gradients, parametres, learning_rate)
+        Af = activations['A' + str(C)]
+
+        # calcul du log_loss et de l'accuracy
+        training_history[i, 0] = (log_loss(y.flatten(), Af.flatten()))
+        y_pred = predict(X, parametres)
+        training_history[i, 1] = (accuracy_score(y.flatten(), y_pred.flatten()))
+
+    # Plot courbe d'apprentissage
+    plt.figure(figsize=(12, 4))
+    plt.subplot(1, 2, 1)
+    plt.plot(training_history[:, 0], label='train loss')
+    plt.legend()
+    plt.subplot(1, 2, 2)
+    plt.plot(training_history[:, 1], label='train acc')
+    plt.legend()
+    plt.show()
+
+    return training_history
+
+
+#%% Partie telecom
 
 # Step 1: Define the necessary parameters for our simulation
 def encodage_matrice(bits, G, k):
@@ -106,8 +224,6 @@ M = BPSK_modulation(mat_combi(G, k))
 
 Eb_No_lin = 10**(1 / 10)  # Convert Eb/N0 values to linear scale # Eb_No_dB = 1  #Eb/N0 values in dB, on prend 1 pour les raisons évoqués dans le papier voir (a) polar code figure vert
 
-#%%
-
 combi_bits = mat().flatten()
 encoded_bits = encodage_matrice(combi_bits, G, k)
 X_train_flat = BPSK_modulation(encoded_bits) # Pas de buit ajouté pour l'instant
@@ -118,7 +234,7 @@ X_train = X_train_flat.reshape(256, 2*k)
 Y_train = result_comp(X_train_flat, G, MAT) # Evidament égale à "mat" mais nous vérfions tout de même
 estimate_signals = np.array(Y_train).flatten()  # Flatten the array from (256, 16) to (1024,)
 
-nb = 1*k
+nb = 3*k
 bits_neur = np.random.randint(0, 2, nb)
 encoded_bits_test = encodage_matrice(bits_neur, G, k)
 X_test_flat = BPSK_modulation(encoded_bits_test)
@@ -133,16 +249,10 @@ estimate_signals = np.array(Y_test).flatten()
 errors = count_errors(bits_neur, estimate_signals)
 print(errors)  # normalement égale à bits_neur car pas de bruit
 
+
+X = X_train.T
+y = Y_train.T
+print('dimensions de X:', X.shape)
+print('dimensions de y:', y.shape)
 #%%
-
-sigma = 0.01  # Adjust noise level as required
-trained_model = train_neural_network(X_train, Y_train, X_test, Y_test, sigma)
-prediction = trained_model.predict(X_test)
-signal_recu = (prediction > 0.5).flatten()
-# Count errors
-errors = count_errors(Y_test.flatten(), signal_recu)
-
-# Calculate BER
-BER = errors/len(Y_test.flatten())
-
-print("BER = ", BER)
+deep_neural_network(X, y)
