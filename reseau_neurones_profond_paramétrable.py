f9e02f064aee1adb27381e7979617028876b649a
clement1938 <52299436+clement1938@users.noreply.github.com>
2024-05-05 14:35:29 +0200

    Add files via upload

---
+++ reseau_neurones_profond_paramétrable.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  4 16:26:05 2024
+
+@author: 33695
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score, log_loss
+from tqdm import tqdm
+
+#%%
+
+def initialisation(dimensions):
+    
+    parametres = {}
+    C = len(dimensions)
+
+    np.random.seed(1)
+
+    for c in range(1, C):
+        parametres['W' + str(c)] = np.random.randn(dimensions[c], dimensions[c - 1])
+        parametres['b' + str(c)] = np.random.randn(dimensions[c], 1)
+
+    return parametres
+
+def forward_propagation(X, parametres): # , activation):
+  
+  activations = {'A0': X}
+
+  C = len(parametres) // 2
+
+  for c in range(1, C + 1):
+
+    Z = parametres['W' + str(c)].dot(activations['A' + str(c - 1)]) + parametres['b' + str(c)]
+    activations['A' + str(c)] = 1 / (1 + np.exp(-Z))
+
+    # if activation == 'sigmoid':
+    #     activations['A' + str(c)] = sigmoid(Z)
+    # if activation == 'ReLU':
+    #     activations['A' + str(c)] = ReLU(Z)
+
+  return activations
+
+# def sigmoid(z):
+#     return 1 / (1 + np.exp(-z))
+
+# def ReLU(z):
+#     if z<0:
+#         return 0
+#     return z
+
+
+def back_propagation(y, parametres, activations):
+
+  m = y.shape[1]
+  C = len(parametres) // 2
+
+  dZ = activations['A' + str(C)] - y
+  gradients = {}
+
+  for c in reversed(range(1, C + 1)):
+    gradients['dW' + str(c)] = 1/m * np.dot(dZ, activations['A' + str(c - 1)].T)
+    gradients['db' + str(c)] = 1/m * np.sum(dZ, axis=1, keepdims=True)
+    if c > 1:
+      dZ = np.dot(parametres['W' + str(c)].T, dZ) * activations['A' + str(c - 1)] * (1 - activations['A' + str(c - 1)])
+
+  return gradients
+
+def update(gradients, parametres, learning_rate):
+
+    C = len(parametres) // 2
+
+    for c in range(1, C + 1):
+        parametres['W' + str(c)] = parametres['W' + str(c)] - learning_rate * gradients['dW' + str(c)]
+        parametres['b' + str(c)] = parametres['b' + str(c)] - learning_rate * gradients['db' + str(c)]
+
+    return parametres
+
+def predict(X, parametres):
+  activations = forward_propagation(X, parametres)
+  C = len(parametres) // 2
+  Af = activations['A' + str(C)]
+  return Af >= 0.5
+
+def deep_neural_network(X, y, hidden_layers = (128, 64, 32), learning_rate = 0.001, n_iter = 3000):
+    
+    # initialisation parametres
+    dimensions = list(hidden_layers)
+    dimensions.insert(0, X.shape[0])
+    dimensions.append(y.shape[0])
+    np.random.seed(1)
+    parametres = initialisation(dimensions)
+
+    # tableau numpy contenant les futures accuracy et log_loss
+    training_history = np.zeros((int(n_iter), 2))
+
+    C = len(parametres) // 2
+
+    # gradient descent
+    for i in tqdm(range(n_iter)):
+
+        activations = forward_propagation(X, parametres)
+        gradients = back_propagation(y, parametres, activations)
+        parametres = update(gradients, parametres, learning_rate)
+        Af = activations['A' + str(C)]
+
+        # calcul du log_loss et de l'accuracy
+        training_history[i, 0] = (log_loss(y.flatten(), Af.flatten()))
+        y_pred = predict(X, parametres)
+        training_history[i, 1] = (accuracy_score(y.flatten(), y_pred.flatten()))
+
+    # Plot courbe d'apprentissage
+    plt.figure(figsize=(12, 4))
+    plt.subplot(1, 2, 1)
+    plt.plot(training_history[:, 0], label='train loss')
+    plt.legend()
+    plt.subplot(1, 2, 2)
+    plt.plot(training_history[:, 1], label='train acc')
+    plt.legend()
+    plt.show()
+
+    return training_history
+
+
+# from sklearn.datasets import make_circles
+# X, y = make_circles(n_samples=100, noise=0.1, factor=0.3, random_state=0)
+# X = X.T
+# y = y.reshape((1, y.shape[0]))
+
+# print('dimensions de X:', X.shape)
+# print('dimensions de y:', y.shape)
+
+# plt.scatter(X[0, :], X[1, :], c=y, cmap='summer')
+# plt.show()
